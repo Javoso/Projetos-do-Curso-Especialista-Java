2bf287b9853eff698414e7a5235a5feb8a3417cb
Javoso <lucasqueirozqxd@gmail.com>
2024-05-05 23:23:33 -0300

    First Commit

---
+++ Fundamentos da linguagem Java/3/Desafio.java
@@ -0,0 +1,7 @@
+public class Desafio {
+    public static void main (String[] args) {
+    	int valor = 1;
+    	int valor2 = 2;
+    	System.out.println("A Soma dos números é = " + (valor+valor2));
+    }
+}
