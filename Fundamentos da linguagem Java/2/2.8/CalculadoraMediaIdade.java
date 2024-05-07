7fb4e7ae02d247feff5b42e9c5d2c306fa12720c
Javoso <lucasqueirozqxd@gmail.com>
2024-05-07 00:34:01 -0300

    2.8 Operadores aritméticos

---
+++ Fundamentos da linguagem Java/2/2.8/CalculadoraMediaIdade.java
@@ -0,0 +1,14 @@
+public class CalculadoraMediaIdade {
+
+   public static void main(String[] args) {
+	int minhaIdade = 35;
+	int suaIdade = 31;
+	int idadeDoJavoso = 27;
+	
+	int idadeMedia = (minhaIdade + suaIdade+ idadeDoJavoso)/3;
+	System.out.println("Idade média: " + idadeMedia);
+	
+    }
+	
+}
+
---
+++ Fundamentos da linguagem Java/2/2.8/OperadoresAritmeticos.java
@@ -0,0 +1,34 @@
+public class OperadoresAritmeticos {
+
+   public static void main(String[] args) {
+	int minhaIdade = 35;
+	int suaIdade = 30;
+	
+	//Adição
+	int totalIdades = minhaIdade + suaIdade;
+	System.out.println("Soma das idades: " + totalIdades);
+	
+	
+	//Subtração
+	int diferencaEntreAsIdades = minhaIdade - suaIdade;
+	System.out.println("Diferença entre as idades: " + diferencaEntreAsIdades);
+	
+	
+	//Multiplicação
+	int dobroDaIdade = 2 * suaIdade;
+	System.out.println("O Dobro da sua idade: " + dobroDaIdade);
+	
+	
+	//Divisão
+	int metadeDaIdade = suaIdade/2;
+	System.out.println("Metade da sua idade: " + metadeDaIdade);
+	
+	
+	//Módulo
+	int restoDaDivisao = 7 % 2;
+	System.out.println("Resto da divisão (módulo): " + restoDaDivisao);
+	
+    }
+	
+}
+
