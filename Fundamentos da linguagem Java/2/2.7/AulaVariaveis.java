f5650695cc41c8025aad337edde117933db9e281
Javoso <lucasqueirozqxd@gmail.com>
2024-05-07 00:11:55 -0300

    2.7 Trabalhando com variáveis

---
+++ Fundamentos da linguagem Java/2/2.7/AulaVariaveis.java
@@ -0,0 +1,17 @@
+public class AulaVariaveis {
+
+   public static void main(String[] args) {
+	//Não recomendado pela Code Conventions da Oracle
+	//int minhaIdade = 27, suaIdade = 20;
+	// lowerCamelCase
+	int minhaIdade = 27;
+	int suaIdade = 30;
+	
+	int totalIdades = minhaIdade + suaIdade;
+	
+	System.out.println("Total Idades: " + totalIdades);
+	
+    }
+	
+}
+
