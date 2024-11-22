06c60c47fc939319d151c0f9be776b11844d0e91
Lucas do Nascimento Queiroz <36884733+Javoso@users.noreply.github.com>
2024-11-22 01:03:39 -0300

    Retirando o comentario do método Main.

--- Fundamentos da linguagem Java/2/2.4/OlaMergulhador.java
+++ Fundamentos da linguagem Java/2/2.4/OlaMergulhador.java
@@ -1,8 +1,6 @@
 //Você pode comentar uma linha inteira
 public class OlaMergulhador {
-/*
 	public static void main(String[] args) {
 		System.out.println("Olá Mergulhador, seja bem-vindo");
 	}
-*/
 }
